48f9c942f474ccc9e2c18af27f67662347dffc61
narayanibokde9 <116104916+narayanibokde9@users.noreply.github.com>
2023-02-03 20:50:12 +0530

    Installed CORS

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
 
 //import routes
 const userRoutes = require('./routes/users')
