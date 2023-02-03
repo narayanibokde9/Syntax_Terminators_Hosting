b347c19fb5339786536a7d23638bd9c0e70d98d9
narayanibokde9 <116104916+narayanibokde9@users.noreply.github.com>
2023-02-04 00:02:50 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,65 +1,24 @@
-// const express = require('express')
-// const {
-// 	show_all_products,
-// 	show_product_by_id,
-// 	search_page,
-// 	filter_page,
-// 	brand_list,
-// 	storage_list
-// } = require('../controllers/productsController')
-
-// const router = express.Router()
-
-// router.get('/showdb', show_all_products)
-
-// router.get('/showPhone/:_id', show_product_by_id)
-
-// router.get('/search', search_page)
-
-// router.get('/filter', filter_page)
-
-// router.get('/brands', brand_list)
-
-// router.get('/storage', storage_list)
-// module.exports = router
-
-const express = require("express");
+const express = require('express')
 const {
 	show_all_products,
 	show_product_by_id,
 	search_page,
 	filter_page,
 	brand_list,
-	storage_list,
-} = require("../controllers/productsController");
+	storage_list
+} = require('../controllers/productsController')
 
-const {
-	get_fav_items,
-	add_fav_item,
-	delete_item,
-} = require("../controllers/favCartController");
+const router = express.Router()
 
-const router = express.Router();
+router.get('/showdb', show_all_products)
 
-router.get("/showdb", show_all_products);
+router.get('/showPhone/:_id', show_product_by_id)
 
-router.get("/showPhone/:_id", show_product_by_id);
+router.get('/search', search_page)
 
-router.get("/search", search_page);
+router.get('/filter', filter_page)
 
-router.get("/filter", filter_page);
+router.get('/brands', brand_list)
 
-router.get("/brands", brand_list);
-
-router.get("/storage", storage_list);
-
-const requireAuth = require("../middleware/requireAuth");
-router.use(requireAuth);
-
-router.get("/favorites", get_fav_items);
-
-router.post("/favorites", add_fav_item);
-
-router.delete("/favorites", delete_item);
-
-module.exports = router;
+router.get('/storage', storage_list)
+module.exports = router
