4ebc2a46c22e9c089762ad264201360ee598f178
narayanibokde9 <116104916+narayanibokde9@users.noreply.github.com>
2023-02-11 19:22:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,23 +13,12 @@ const productRoutes = require('./routes/products')
 
 const app = express()
 
-// app.use(
-// 	cors({
-// 		origin: "https://syntax-terminators-hosting.vercel.app",
-// 		optionsSuccessStatus: 200,
-// 	})
-// );
-
-app.use((req, res, next) => {
-  const allowedOrigins = ["http://localhost:3000", "https://syntax-terminators-hosting.vercel.app"];
-  const origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+app.use(
+	cors({
+		origin: "https://syntax-terminators-hosting.vercel.app",
+		optionsSuccessStatus: 200,
+	})
+);
 
 app.use(express.json())
 app.use((req, res, next) => {
