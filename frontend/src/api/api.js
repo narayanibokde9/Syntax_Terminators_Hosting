99c27af64cb56974c2f10b7ea3e4a9cf352a0da3
narayanibokde9 <116104916+narayanibokde9@users.noreply.github.com>
2023-04-22 21:23:23 +0530

    New Changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
-// const url_proxy = "https://syntax-terminators-hosting-api.vercel.app/";
+const url_proxy = "https://syntax-terminators-hosting-api.vercel.app/";
 
-const url_proxy = "http://localhost:4000/";
+// const url_proxy = "http://localhost:4000/";
 
 export default url_proxy;
