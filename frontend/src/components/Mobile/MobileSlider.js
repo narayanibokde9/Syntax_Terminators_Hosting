d64ec2061110521f0cfebe9b01b8cf5bad697652
narayanibokde9 <116104916+narayanibokde9@users.noreply.github.com>
2023-04-22 22:41:57 +0530

    Update MobileSlider.js

--- frontend/src/components/Mobile/MobileSlider.js
+++ frontend/src/components/Mobile/MobileSlider.js
@@ -2,30 +2,25 @@ import { React, useEffect, useState } from "react";
 
 import MobileCardTemp from "./MobileCardTemp";
 
+import Backdrop from "@mui/material/Backdrop";
+import { CircularProgress } from "@mui/material";
+
 import Slider from "react-slick";
 import url_proxy from "../../api/api";
-// const url_proxy = "https://syntax-terminators-hosting-api.vercel.app/";
 
 export default function MobileSlider() {
 	const [product, setProduct] = useState([]);
-
-	// 	useEffect(() => {
-	// 	console.log("GOT URL");
-	// 	fetch(`${url_proxy}products/showdb`)
-	// 		.then((res) => {
-	// 			res.json();
-	// 			console.log("GOT JSON");
-	// 		})
-	// 		.then((jsonRes) => {
-	// 			console.log(jsonRes);
-	// 			setProduct(jsonRes);
-	// 		});
-	// }, []);
+	const [isLoading, setIsLoading] = useState(true);
+	const [open, setOpen] = useState(true);
 
 	useEffect(() => {
 		fetch(`${url_proxy}products/showdb`)
 			.then((res) => res.json())
-			.then((jsonRes) => setProduct(jsonRes));
+			.then((jsonRes) => {
+				setProduct(jsonRes);
+				setIsLoading(false);
+				setOpen(!open);
+			});
 	}, []);
 
 	const settings = {
@@ -38,17 +33,31 @@ export default function MobileSlider() {
 	return (
 		<div className="m-4">
 			<h1 className="text-2xl">Mobiles</h1>
-			<Slider {...settings} className=" -mb-16 m-8 ">
-				{product &&
-					product.map((mobile) => {
-						const { _id } = mobile;
-						return (
-							<div className="px-2">
-								<MobileCardTemp {...mobile} key={_id} />
-							</div>
-						);
-					})}
-			</Slider>
+			{isLoading ? (
+				<Backdrop
+					sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+					open={open}
+				>
+					<CircularProgress color="inherit" />
+					<div className="px-5">
+						<h5 class="truncate capitalize text-xl font-semibold tracking-tight text-white dark:text-white hover:text-red-600">
+							Loading...
+						</h5>
+					</div>
+				</Backdrop>
+			) : (
+				<Slider {...settings} className=" -mb-16 m-8 ">
+					{product &&
+						product.map((mobile) => {
+							const { _id } = mobile;
+							return (
+								<div className="px-2">
+									<MobileCardTemp {...mobile} key={_id} />
+								</div>
+							);
+						})}
+				</Slider>
+			)}
 		</div>
 	);
 }
